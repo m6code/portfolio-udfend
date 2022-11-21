a6fba22b13535c10c018990280892f4f0bdcb7fd
Benjamin SA <benjamynfrank101@gmail.com>
2022-11-21 03:58:43 +0100

    [Mon 2022-11-21 03:58:43 WAT] Switching to tailwindcss and Vuejs

---
+++ portfolio_v2/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h1 class="text-2xl text-green-700">{{message}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "App",
+    data() {
+      return {
+        message: "Hello Vue don Land"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ portfolio_v2/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html",
+    "./src/*.vue",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
